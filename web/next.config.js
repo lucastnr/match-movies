308838c7fe677a43a90f058343eeb1c2a005fb61
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2024-06-23 14:33:52 -0300

    chore: cleanup

--- web/next.config.js
+++ web/next.config.js
@@ -8,14 +8,11 @@ const { composePlugins, withNx } = require('@nx/next');
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
 };
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
   withNx,
 ];
 
